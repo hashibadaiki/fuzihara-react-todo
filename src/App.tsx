ddac151b38ff77e9fe07f7cfe3d5febf6295f8e3
hashibadaiki <higemeganeking@gmail.com>
2021-12-19 17:45:02 +0900

    getStateの処理を実装した

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { InsertFrom } from "./InsertForm"
 import { useSelector, useDispatch } from 'react-redux';
 import { pulsA, pulsB, pulsC, pulsD } from './asyncSlice';
 import { RootState } from './store';
+import { store } from './store';
 
 export const App = () => {
-  const count = useSelector((state: RootState) => state.counter.count);
   const text = useSelector((state: RootState) => state.counter.text);
   const countA = useSelector((state: RootState) => state.asyncTest.countA);
   const countB = useSelector((state: RootState) => state.asyncTest.countB);
@@ -16,6 +16,18 @@ export const App = () => {
   const testC = () => {
     dispatch(pulsA())
     dispatch(pulsC(countA))
+  }
+
+  // getStateだと想定通りに対応できる
+  const testE = () => {
+    // console.log(store.getState())
+    dispatch(pulsA())
+    const nowStateA = store.getState()
+    dispatch(pulsC(nowStateA.asyncTest.countA))
+
+    // この書き方はだめ
+    // const countA = useSelector((state: RootState) => state.asyncTest.countA);
+    // dispatch(pulsC(countA))
   }
 
   return (
@@ -30,6 +42,8 @@ export const App = () => {
         <div>{countC}</div>
         <button onClick={() => dispatch(pulsD())}>Dup</button>
         <div>{countD}</div>
+        <button onClick={() => testE()}>Eup</button>
+        <div>{countD}e</div>
         {/* <button onClick={() => dispatch(pulsC())}>BothUP(normal)</button>
         <button onClick={() => dispatch(awaitTest())}>BothUP(async)</button> */}
       </div>
